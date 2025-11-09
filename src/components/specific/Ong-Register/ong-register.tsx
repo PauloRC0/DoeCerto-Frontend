2cfe8bb91a51f52c3c51a7a91d005df7fb13cf83
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-11-09 19:20:23 -0300

    style: change text color

--- src/components/specific/Ong-Register/ong-register.tsx
+++ src/components/specific/Ong-Register/ong-register.tsx
@@ -75,7 +75,7 @@ export default function OngRegisterPage() {
                     
                     <p className="text-base text-right font-bold -mt-4 mb-4"> 
                         Já possui conta?{" "}
-                        <Link href="/login" className="font-bold text-white">
+                        <Link href="/login" className="font-bold text-[#E0C4FF]">
                             Fazer Login
                         </Link>
                     </p>
