9fed154f25fc75e656d2d618571553c1a3cd8116
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-11-09 19:13:42 -0300

    style: remove underline

--- src/components/specific/Ong-Register/ong-register.tsx
+++ src/components/specific/Ong-Register/ong-register.tsx
@@ -75,7 +75,7 @@ export default function OngRegisterPage() {
                     
                     <p className="text-base text-right font-bold -mt-4 mb-4"> 
                         Já possui conta?{" "}
-                        <Link href="/login" className="underline font-bold text-white hover:text-purple-300">
+                        <Link href="/login" className="font-bold text-white">
                             Fazer Login
                         </Link>
                     </p>
