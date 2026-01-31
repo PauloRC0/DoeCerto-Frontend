7acd71614150e2b1f6ebf6a121b8c12bb8f57a34
GuilhermeMth <guilhermemfranca06@gmail.com>
2026-01-31 17:37:47 -0300

    refactor: remove unused environment variable and entrypoint script; update API URL references

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Substitui o placeholder pelo valor real da variável de ambiente
-find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|__NEXT_PUBLIC_API_URL__|${NEXT_PUBLIC_API_URL}|g" {} +
-find /usr/share/nginx/html -type f -name "*.html" -exec sed -i "s|__NEXT_PUBLIC_API_URL__|${NEXT_PUBLIC_API_URL}|g" {} +
-
-# Chama o entrypoint original do nginx
-exec /docker-entrypoint.sh nginx -g "daemon off;"
